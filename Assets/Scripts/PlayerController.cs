ce53c7a1e48f0f94bb1ce6adedecc8cc3c4bb809
Renanmc132 <renanmc100@gmail.com>
2026-06-10 14:03:10 -0300

    feat : Adciona movimentação e animação do Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Rigidbody2D _rb;
+    private Animator _anim;
+    private float moveSpeed = 5f;
+    private Vector2 direction;
+    
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        _rb.MovePosition(_rb.position + direction.normalized * moveSpeed * Time.deltaTime);
+
+        if(direction.sqrMagnitude > 0.1)
+        {
+            _anim.SetFloat("XInput", direction.x);
+            _anim.SetFloat("YInput", direction.y);
+
+            _anim.SetBool("Moviment", true);
+        }
+        else
+        {
+            _anim.SetBool("Moviment", false);
+        }
+        
+    }
+
+
 }
