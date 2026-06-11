1a8583e992e171faa2d29e9efa665c462b0b4ee8
Renanmc132 <renanmc100@gmail.com>
2026-06-11 16:52:01 -0300

    fix : Corrigi bug da animação da calça

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,10 @@ public class PlayerController : MonoBehaviour
     public GameObject _shirts;
     private int types = 1;
 
+    [Header("Pants Variables")]
+    private Animator _pantsAnim;
+    public GameObject _pants;
+
     [Header("Movement")]
     private float moveSpeed = 5f;
     private Vector2 direction;
@@ -24,6 +28,7 @@ public class PlayerController : MonoBehaviour
         _anim = GetComponent<Animator>();
         _spr = GetComponent<SpriteRenderer>();
         _shirtAnim = _shirts.GetComponent<Animator>();
+        _pantsAnim = _pants.GetComponent<Animator>();
     }
 
 
@@ -51,6 +56,8 @@ public class PlayerController : MonoBehaviour
             _anim.SetFloat("YInput", direction.y);
             _shirtAnim.SetFloat("XInput", direction.x);
             _shirtAnim.SetFloat("YInput", direction.y);
+            _pantsAnim.SetFloat("XInput", direction.x);
+            _pantsAnim.SetFloat("YInput", direction.y);
 
             lastDirection = direction;
 
@@ -58,14 +65,18 @@ public class PlayerController : MonoBehaviour
             _anim.SetFloat("LastY", lastDirection.y);
             _shirtAnim.SetFloat("LastX", lastDirection.x);
             _shirtAnim.SetFloat("LastY", lastDirection.y);
+            _pantsAnim.SetFloat("LastX", lastDirection.x);
+            _pantsAnim.SetFloat("LastY", lastDirection.y);
 
             _anim.SetBool("Moviment", true);
             _shirtAnim.SetBool("Moviment", true);
+            _pantsAnim.SetBool("Moviment", true);
         }
         else
         {
             _anim.SetBool("Moviment", false);
             _shirtAnim.SetBool("Moviment", false);
+            _pantsAnim.SetBool("Moviment", false);
         }
     }
 
